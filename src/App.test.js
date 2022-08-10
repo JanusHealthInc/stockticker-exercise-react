72478fac7897f66fdae3cf0f9d5a48f8133caf64
Cole <colepanike@gmail.com>
2022-08-09 22:06:25 -0500

    🧪 Set up initial test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // I want to issue some ticker events here, but with the implementation being
+  // though window, I don't feel this exercise is intended to merit that degree
+  // of integration with the hypothetical ticker. Just know that I would
+  // normally test that way. Here I'll simply test off known information.
+
+  // App ticker values for each of these cases
+  const hypotheticalTickers = ["S&P 500", "DOW J", "NASDAQ"];
+
+  hypotheticalTickers.forEach((ticker) => {
+    // Make sure each of the contrived stocks is at least represented on the page
+    // Should be only on the page once though (Going for "more refined" solution)
+    expect(screen.getAllByText(ticker)).toHaveLength(1);
+  });
 });
